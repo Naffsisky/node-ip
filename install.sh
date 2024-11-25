7d61bd9dcc88abe9bba880100d827249eb4cb7e9
Nap <70518391+Naffsisky@users.noreply.github.com>
2024-11-25 20:24:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,7 @@ sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.g
 echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
 sudo apt-get update
 sudo apt-get install k6
+
+# Deploy Docker
+docker build -t api-image /src
+docker run -d -p 5077:5077 --restart=always --name  api-server api-image
